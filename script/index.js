ef0654d63feeceb8c20bf746fbaa0bdb979a4753
Tobias Lopez <tobiasl38@hotmail.fr>
2023-06-17 14:04:19 +0200

    updates

--- script/index.js
+++ script/index.js
@@ -16,13 +16,30 @@ const darkMode = document.querySelector(".dark-mode")
 const bodyElement = document.querySelector("body")
 const darkElement = document.querySelectorAll(".dark-el")
 
-darkMode.addEventListener("click", setDark)
+// Fonction pour activer ou désactiver le mode sombre et enregistrer son état
+function toggleDarkMode() {
+    darkMode.classList.toggle("dark");
+    bodyElement.classList.toggle("dark");
+    darkElement.forEach(element => element.classList.toggle("dark"));
 
-function setDark() {
-    darkMode.classList.toggle("dark")
-    bodyElement.classList.toggle("dark")
-    darkElement.forEach(element => element.classList.toggle("dark"))
+    // Enregistrement de l'état du mode sombre dans le stockage local
+    const isDarkModeEnabled = bodyElement.classList.contains("dark");
+    localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
 }
+
+// Ajouter un écouteur d'événement sur le bouton darkMode
+darkMode.addEventListener("click", toggleDarkMode);
+
+// Vérifier l'état du mode sombre lors du chargement de la page
+function checkDarkMode() {
+    const isDarkModeEnabled = localStorage.getItem("darkModeEnabled");
+    if (isDarkModeEnabled === "true") {
+    toggleDarkMode();
+    }
+}
+
+// Appeler la fonction checkDarkMode() lorsque la page a fini de charger
+window.addEventListener("DOMContentLoaded", checkDarkMode);
 
 function animOnLoad() {
     gsap.fromTo(".transfRight", { x: -100 }, { x: 0, duration: 1, ease: "back", stagger: .1 })
