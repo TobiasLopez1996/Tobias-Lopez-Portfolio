124d1db571081a2de5767583909fb4d984456eeb
Tobias Lopez <tobiasl38@hotmail.fr>
2023-06-25 16:55:56 +0200

    updates

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-import { gsap } from "Tobias-Lopez-Portfolio/node_modules/gsap/src/index.js"
+import { gsap } from "../node_modules/gsap/src/index.js"
 
 const hamburgerButton = document.querySelector(".nav-toggler")
 const navigation = document.querySelector("nav")
